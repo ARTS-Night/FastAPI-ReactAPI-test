695cf85b3089fe13ccfbc0b5188419b7d92beebf
ARTS <a.rtspg01@gmail.com>
2026-04-15 10:18:49 +0900

    version 2のテキストをAppコンポーネントに追加

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', padding: 20 }}>
       <h1>Simple React + Docker (Node)</h1>
+      <p>version 2</p>
       <p>開発サーバが動作しています。</p>
     </div>
   )
