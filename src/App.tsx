40857c7ff3f4dbc6959f631455038c4db791490c
ARTS <a.rtspg01@gmail.com>
2026-04-20 10:17:46 +0900

    v4

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App(): JSX.Element {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', padding: 20 }}>
       <h1>Simple React(ts) + Docker (Node)</h1>
-      <p>V3</p>
+      <p>V4</p>
       <p>pnpm version</p>
       <p>github自動デプロイテスト</p>
       <p>開発サーバが動作しています。</p>
